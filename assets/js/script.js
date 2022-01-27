e669e2472762501135fc497d7ca600e5542090d8
enzo metmer <metmerenzi@gmail.com>
2022-01-27 16:20:28 +0100

    Dernier commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 let rsl = document.querySelector('#rsl');
 let crl = document.querySelector('#crl');
 let kvn = document.querySelector('#kvn');
-let msgScore = document.querySelector('#msgScore')
+let msgScore = document.querySelector('#msgScore');
+let countCpu = 1;
+let countPlayer = 1;
+let pointPlayer = document.querySelector('#pointPlayer');
+let pointCpu = document.querySelector('#pointCpu');
 
+// let pointPlayers = () => {
+//     if (cpuChoice == 0 && playerChoice == 1 || cpuChoice == 1 && ) {
+        
+//     } else {
+        
+//     }
+// }
 
 rsl.addEventListener('click', () => {
     playerChoice = 1
@@ -13,8 +24,10 @@ rsl.addEventListener('click', () => {
         msgScore.innerHTML = 'égalité';
     } else if (cpuChoice == 2 ) { 
         msgScore.innerHTML = 'Russell a gagné contre Carl';
+        pointPlayer.innerHTML = countPlayer++;
     } else {
         msgScore.innerHTML = 'Russel a perdu contre Kevin';
+        pointCpu.innerHTML = countCpu++;
     }
     
 });
@@ -28,8 +41,10 @@ crl.addEventListener('click', () => {
         msgScore.innerHTML = 'égalité';
     } else if (cpuChoice == 3 ) { 
         msgScore.innerHTML = 'Carl a gagné contre Kevin';
+        pointPlayer.innerHTML = countPlayer++;
     } else {
         msgScore.innerHTML = 'Carle a perdu contre Russell';
+        pointCpu.innerHTML = countCpu++;
     }
 });
 
@@ -43,8 +58,17 @@ kvn.addEventListener('click', () => {
         msgScore.innerHTML = 'égalité';
     } else if (cpuChoice == 1 ) { 
         msgScore.innerHTML = 'Kevin a gagné contre Russell';
+        pointPlayer.innerHTML = countPlayer++;
     } else {
         msgScore.innerHTML = 'Kevin a perdu contre Carl';
+        pointCpu.innerHTML = countCpu++;
     }
 });
+
+
+
+
+
+
+
 
